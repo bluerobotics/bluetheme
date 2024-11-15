5e7f7fa46d78885206b959ac94cb7f0cc930c600
Ricardo Pascal <pascal@bluerobotics.com>
2024-11-15 11:12:24 +0100

    first version of bluetheme (#2)

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./node_modules/flowbite/**/*.js",
+        "./static/**/*.js",
+        "./templates/*.html",
+        "./templates/**/*.html",
+    ],
+    theme: {
+        colors: {
+            'green-kelp': '#4fa483',
+            'blue-br': '#2699d0',
+            'blue-mariner': '#135da3',
+        },
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin'),
+        require('flowbite-typography'),
+    ]
+}
+
